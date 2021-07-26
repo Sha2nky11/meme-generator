57f53af533de70b3298ffa32000fd7d19cf64d70
sha2nk <shashankyadav488@gmail.com>
2021-07-27 00:01:52 +0530

    app component css updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React ,{ Component } from 'react';
+import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      memelimit:10
+    }
+  }
+
   render() {
     return (
       <div className="App">
           <h2> Welcome to Meme Generator </h2>
+          { 
+          this.props.memes.slice(0,this.state.memelimit).map((meme,index) => {
+            return(
+               <h4 key={index}> {meme.name} </h4>
+             ) 
+           })
+           }
+           <Button className="meme-button" onClick={() => this.setState({memelimit:this.state.memelimit+10})}>
+              Load 10 more memes......
+           </Button>
       </div>
     );
   }
